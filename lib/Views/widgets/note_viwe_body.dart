6e3e51a144d49e64383a277387e02cfab0efae5e
mohamed hassan <63217614+Moamed1010@users.noreply.github.com>
2025-11-29 18:05:18 +0200

    creeate cutom note Item

--- lib/Views/widgets/note_viwe_body.dart
+++ lib/Views/widgets/note_viwe_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/Views/widgets/custom_app_bar.dart';
+import 'package:note_app/Views/widgets/custom_note_item.dart';
 
 class NoteViweBody extends StatelessWidget {
   const NoteViweBody({super.key});
@@ -7,46 +10,14 @@ class NoteViweBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(children: [const SizedBox(height: 10), CustomAppBar()]),
-    );
-  }
-}
-
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          "Notes",
-          style: TextStyle(
-            fontSize: 28,
-            color: Colors.white,
-            decoration: TextDecoration.none,
-          ),
-        ),
-        Spacer(),
-        const CustomSearchIcon(),
-      ],
-    );
-  }
-}
-
-class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 46,
-      width: 46,
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(.07),
-        borderRadius: BorderRadius.circular(16),
+      child: Column(
+        children: [
+          const SizedBox(height: 10),
+          CustomAppBar(),
+          SizedBox(height: 10),
+          NoteItem(),
+        ],
       ),
-      child: const Center(child: Icon(Icons.search, size: 28)),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class NoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+      theme: ThemeData(brightness: Brightness.dark,fontFamily: 'Poppins'),
       home: const NoteView(),
     );
   }
