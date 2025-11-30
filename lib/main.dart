ccafd9114e3476f8aef3c6ede075434248a6146d
mohamed hassan <63217614+Moamed1010@users.noreply.github.com>
2025-11-30 18:51:20 +0200

    `Added KNotesBox constant and Hive initialization to main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/Constant.dart';
 import 'package:note_app/Views/note_view.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox(KNotesBox);
   runApp(NoteApp());
 }
 
